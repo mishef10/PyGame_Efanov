692e692f11fe6583713baa12ff6a0867b43800e8
mishef10 <mishef10@mail.ru>
2014-11-18 23:22:18 +1000

    Сделал 5 задание,1 часть 6-ого

--- Bars2.py
+++ Bars2.py
@@ -6,6 +6,7 @@ class Block:
         self.width = width
         self.height = height
         self.color = color
+        self.color2 = (100,0,0)
         self.image = pygame.Surface((width, height))
         self.draw()
         self.rect = self.image.get_rect()
@@ -19,19 +20,21 @@ class Block:
     def events(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                # flag=True
                 if self.collidePoint(event.pos[0], event.pos[1]) == True:
                     self.drag = True
                     print("Flag True")
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.drag = False
         if event.type==pygame.MOUSEMOTION:
             if self.drag==True:
-                self.moveON(event.rel[0],event.rel[0])
-
+                self.moveON(event.rel[0],event.rel[1])
 
     def collidePoint(self,mouse_x, mouse_y):
-        if self.x<mouse_x<self.x+self.width and self.y<mouse_y<self.y+self.height:
-            flag2=True
-            print("True")
+
+        if self.rect.x<mouse_x<self.rect.x+self.width and self.rect.y<mouse_y<self.rect.y+self.height:
+            color=self.color
+            self.color=self.color2
+            self.color2=color
             return True
         else:
             print("False")
@@ -40,6 +43,8 @@ class Block:
     def moveON(self,move_x,move_y):
         self.rect.x+=move_x
         self.rect.y+=move_y
+
+
 
     def render(self,screen):
         screen.blit(self.image,self.rect)
@@ -54,7 +59,8 @@ while x!=1:
     for e in pygame.event.get():
 
         # print(e)
-
+        if e.type == pygame.MOUSEBUTTONUP:
+            block1.events(e)
         if e.type == pygame.QUIT:
             x=1
         if e.type == pygame.MOUSEBUTTONDOWN:
@@ -63,6 +69,7 @@ while x!=1:
             block1.events(e)
 
     display.fill((55,00,99))
+    block1.draw()
     block1.render(display)
     # block2.render(display)
 
